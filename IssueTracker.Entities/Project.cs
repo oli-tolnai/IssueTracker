8ab67ca884b0502ee18ef1aca906a0999120be9c
oli-tolnai <oli.tolnai3@gmail.com>
2024-12-11 22:16:19 +0100

    project done

--- IssueTracker.Entities/Project.cs
+++ IssueTracker.Entities/Project.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,26 @@ namespace IssueTracker.Entities
 {
     public class Project
     {
+        public Project(string name, string description)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            Description = description;
+        }
+
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-        
+
+        [StringLength(50)]
         public string Name { get; set; }
-    
+
+        [StringLength(200)]
         public string Description { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Issue>? Issues { get; set; }
 
     }
 }
