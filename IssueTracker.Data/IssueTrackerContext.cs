d8dde3eb05ea7e15b055be965fc27e52b50a4e63
oli-tolnai <oli.tolnai3@gmail.com>
2024-12-11 22:24:34 +0100

    dbcontext done

--- IssueTracker.Data/IssueTrackerContext.cs
+++ IssueTracker.Data/IssueTrackerContext.cs
@@ -1,10 +1,31 @@
 ﻿using IssueTracker.Entities;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace IssueTracker.Data
 {
-    public class IssueTrackerContext
+    public class IssueTrackerContext : DbContext
     {
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Issue> Issues { get; set; }
+
+        public IssueTrackerContext(DbContextOptions<IssueTrackerContext> ctx)
+            :base(ctx)
+        {
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.Issues)
+                .WithOne(i => i.Project)
+                .HasForeignKey(i => i.ProjectId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
