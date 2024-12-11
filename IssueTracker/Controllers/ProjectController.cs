fb8f0f214a1c75afa29f482e9a8d84f770ce0cc3
oli-tolnai <oli.tolnai3@gmail.com>
2024-12-11 22:42:18 +0100

    test project controller

---
+++ IssueTracker/Controllers/ProjectController.cs
@@ -0,0 +1,24 @@
+﻿using IssueTracker.Data;
+using IssueTracker.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTracker.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        IssueTrackerContext ctx;
+        public ProjectController(IssueTrackerContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        [HttpPost]
+        public void AddProject(Project project)
+        {
+            ctx.Projects.Add(project);
+            ctx.SaveChanges();
+        }
+    }
+}
