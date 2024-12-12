76c753b9baa0b94f48d8ac492c8f34628d654076
oli-tolnai <oli.tolnai3@gmail.com>
2024-12-12 19:22:32 +0100

    AutoMapper init

---
+++ IssueTracker.Logic/DtoProvider.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using IssueTracker.Entities;
+using IssueTracker.Entities.Dtos.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Logic
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Project, ProjectShortViewDto>();
+                cfg.CreateMap<Project, ProjectViewDto>();
+                cfg.CreateMap<ProjectCreateUpdateDto, Project>();
+            });
+
+            Mapper = new Mapper(config);
+        }
+    }
+}
