d2f9b62e148bd3872a8a7578cc47b0bc4d3d8611
oli-tolnai <oli.tolnai3@gmail.com>
2024-12-12 16:09:14 +0100

    IIdEntity added

---
+++ IssueTracker.Data/Repository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Data
+{
+    public class Repository<T> where T : class
+    {
+        IssueTrackerContext ctx;
+
+        public Repository(IssueTrackerContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T entity)
+        {
+            ctx.Set<T>().Add(entity);
+            ctx.SaveChanges();
+        }
+
+        public T FindById(string id)
+        {
+            return null;
+        }
+    }
+}
--- IssueTracker/Controllers/ProjectController.cs
+++ IssueTracker/Controllers/ProjectController.cs
@@ -8,17 +8,6 @@ namespace IssueTracker.Endpoint.Controllers
     [ApiController]
     public class ProjectController : ControllerBase
     {
-        IssueTrackerContext ctx;
-        public ProjectController(IssueTrackerContext ctx)
-        {
-            this.ctx = ctx;
-        }
 
-        [HttpPost]
-        public void AddProject(Project project)
-        {
-            ctx.Projects.Add(project);
-            ctx.SaveChanges();
-        }
     }
 }
