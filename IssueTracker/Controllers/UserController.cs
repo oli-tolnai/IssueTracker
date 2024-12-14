c03439101a5aa0fb896a7a7c120bc98a6eb2ed64
oli-tolnai <oli.tolnai3@gmail.com>
2024-12-14 17:54:02 +0100

    userInputDto created

---
+++ IssueTracker/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTracker.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public void Register()
+        {
+            //var user = new IdentityUser("geza");
+
+
+        }
+    }
+}
